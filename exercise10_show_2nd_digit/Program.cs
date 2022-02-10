a95d94b7e8b770546858257afec07a20bc6f2027
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-10 19:07:52 +0300

    Added solved exercise 10

---
+++ exercise10_show_2nd_digit/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+//That solution option with a string and a char variables
+
+int randomNumber = new Random().Next(100, 1000);
+string numberStr = randomNumber.ToString("G");  // General format specifier
+char two = numberStr[1];
+
+Console.Write("The number is ");
+Console.ForegroundColor = ConsoleColor.Blue; //Colored output
+Console.WriteLine(numberStr);
+Console.ResetColor();
+
+Console.Write("The second digit is ");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(two);
+Console.ResetColor();
