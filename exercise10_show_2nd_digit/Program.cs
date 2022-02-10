ee80de819d523063bc06ce46530d5d9b287d0ffd
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-10 19:15:47 +0300

    Rewrote the code in math solution in exercise 10

--- exercise10_show_2nd_digit/Program.cs
+++ exercise10_show_2nd_digit/Program.cs
@@ -1,25 +1,25 @@
 ﻿// Показать вторую цифру трёхзначного числа
 
-/* //This is the variant with int variables and division with remainder
+//This is the variant with int variables and division with remainder
 
-int rndmNmbr = new Random().Next(100, 1000);
-int dig1 = rndmNmbr / 10;
-int dig2 = dig1 % 10;
+int rndm = new Random().Next(100, 1000);
+//int dig1 = rndmNmbr / 10;
+int dig = (rndm / 10) % 10;
 
 Console.Write("The number is ");
 Console.ForegroundColor = ConsoleColor.Green; //Colored output
-Console.WriteLine(rndmNmbr);
+Console.WriteLine(rndm);
 Console.ResetColor();
 
 Console.Write("The second digit is ");
 Console.ForegroundColor = ConsoleColor.Red;
-Console.WriteLine(dig2);
-Console.ResetColor(); */
+Console.WriteLine(dig);
+Console.ResetColor(); 
 
 
 //That solution option with a string and a char variables
 
-int randomNumber = new Random().Next(100, 1000);
+/* int randomNumber = new Random().Next(100, 1000);
 string numberStr = randomNumber.ToString("G");  // General format specifier
 char two = numberStr[1];
 
@@ -31,4 +31,4 @@ Console.ResetColor();
 Console.Write("The second digit is ");
 Console.ForegroundColor = ConsoleColor.Magenta;
 Console.WriteLine(two);
-Console.ResetColor();
+Console.ResetColor(); */
