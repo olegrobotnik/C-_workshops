547d952a2c394e4de1473f7933bb80b0bb0b4b31
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-10 18:03:50 +0300

    Added solved exercise24

---
+++ exercise24_cubes_1_to_N/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("Find cubes of numbers from 1 to N");
+Console.Write("Please enter a number: ");
+int N = int.Parse(Console.ReadLine());
+N = Math.Abs(N);
+int n = 1;
+
+Console.WriteLine("Num\tCube");
+while (n <= N)
+{
+    Console.WriteLine($"{n}\t{n * n * n}\t");
+    n += 1; 
+}
