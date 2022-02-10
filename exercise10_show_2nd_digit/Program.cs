61cb1748f1fc1c323ea1efbd995734be62fed2d3
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-10 19:09:45 +0300

    Added solution option to exercise 10

--- exercise10_show_2nd_digit/Program.cs
+++ exercise10_show_2nd_digit/Program.cs
@@ -1,5 +1,22 @@
 ﻿// Показать вторую цифру трёхзначного числа
 
+/* //This is the variant with int variables and division with remainder
+
+int rndmNmbr = new Random().Next(100, 1000);
+int dig1 = rndmNmbr / 10;
+int dig2 = dig1 % 10;
+
+Console.Write("The number is ");
+Console.ForegroundColor = ConsoleColor.Green; //Colored output
+Console.WriteLine(rndmNmbr);
+Console.ResetColor();
+
+Console.Write("The second digit is ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(dig2);
+Console.ResetColor(); */
+
+
 //That solution option with a string and a char variables
 
 int randomNumber = new Random().Next(100, 1000);
