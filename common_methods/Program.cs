dfae6beaa711f61f9933e08486b9d64e952ca297
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-12 10:33:40 +0300

    Added common_methods folder

---
+++ common_methods/Program.cs
@@ -0,0 +1,20 @@
+﻿// See common methods
+
+int Inpt(string msg)                    // Метод ввода числа в терминале с проверкой введённых символов
+{
+    Console.Write(msg);
+    int rslt;
+    string str = Console.ReadLine();
+    bool succ = int.TryParse (str, out rslt);
+    if (succ)
+    {
+        return rslt;
+    }    
+    else 
+    {
+        Console.WriteLine($"Please enter valid number. '{str}' is not a number.");
+    }
+    return rslt;
+}
+
+
