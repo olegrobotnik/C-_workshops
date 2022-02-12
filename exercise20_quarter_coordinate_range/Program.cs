b10f2163841ee59763f4eeef449f9090a4400226
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-12 10:00:45 +0300

    Added console input method with check-up

--- exercise20_quarter_coordinate_range/Program.cs
+++ exercise20_quarter_coordinate_range/Program.cs
@@ -6,31 +6,45 @@ int qrtr = Inpt("Enter a quarter number: ");
 QrtrShwr(qrtr);
 
 
-int Inpt(string msg)
+int Inpt(string msg)                    // Метод ввода целого числа в терминале с проверкой введённых символов
 {
-    Console.Write(msg);
-    int nmbr = int.Parse(Console.ReadLine());
-    return nmbr;
-}
+    int rslt = 0;
+    bool succ = false;
+    while (succ == false)
+    {
+        Console.Write(msg);
+        string inpt = Console.ReadLine();
+        //bool succ = int.TryParse (inpt, out rslt);
+        if (int.TryParse(inpt, out rslt))
+        {
+            succ = 0 < rslt && rslt <= 4;
+        }
+        else
+        {
+            Console.WriteLine($"Enter avalid number of a quarter. '{inpt}' is not a number.");
+        }
+    } 
+    return rslt;
+}   
 
 
 void QrtrShwr(int qrtr)
 {
     if (qrtr == 1)
     {
-        Console.WriteLine("The quarter I. Coordinate range x > 0, y > 0");
+        Console.WriteLine("You've entered quarter I. Coordinate range x > 0, y > 0");
     }
     else if (qrtr == 2)
     {
-        Console.WriteLine("The quarter II. Coordinate range x < 0, y > 0");
+        Console.WriteLine("You've entered quarter II. Coordinate range x < 0, y > 0");
     }
     else if (qrtr == 3)
     {
-        Console.WriteLine("The quarter III. Coordinate range x < 0, y < 0");
+        Console.WriteLine("The entered quarter III. Coordinate range x < 0, y < 0");
     }
     else if (qrtr == 4)
     {
-        Console.WriteLine("The quarter IV. Coordinate range x > 0, y < 0");
+        Console.WriteLine("The entered quarter IV. Coordinate range x > 0, y < 0");
     }
     else
     {
