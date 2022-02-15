ab57d30f49dc152e182431f50c38df17fa7f0bca
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-15 22:50:57 +0300

    Added the method which allows input of the double number

--- exercise26_exponentiation_loop/Program.cs
+++ exercise26_exponentiation_loop/Program.cs
@@ -4,24 +4,37 @@ Console.Clear();
 
 Console.WriteLine("An entered random number is raised to an entered power which is the natural number.");
 
-int A = InptA("Enter the number to be raised to a power: ");
+double A = InptA("Enter a number to be raised to a power: ");
 int B = InptB("Enter a power that a number to be raised to: ");
 
-Console.WriteLine(A + " was raised to " + B);
-//Console.WriteLine(B);
-int exp = Exponentiation(A, B);
+Console.Write("The number ");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.Write(A);
+Console.ResetColor();
+Console.Write(" raised to the power ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write(B); 
+Console.ResetColor();
+Console.WriteLine(".");
+
+double exp = Exponentiation(A, B);
+
+Console.Write($"The result is ");
+Console.ForegroundColor = ConsoleColor.Green;
 Console.Write(exp);
+Console.ResetColor();
+Console.Write(".");
 
 
-int InptA(string msg)                    // Метод ввода числа в терминале с проверкой введённых символов
+double InptA(string msg)                    // Метод ввода числа в терминале с проверкой введённых символов
 {
     Console.Write(msg);
-    int rslt;
     string str = Console.ReadLine();
-    bool succ = int.TryParse (str, out rslt);
-    if (succ)
+    double rslt = 0;
+    bool succBool = double.TryParse(str, out rslt);
+    if (succBool)
     {
-        return rslt;
+        return rslt; 
     }    
     else 
     {
@@ -40,7 +53,7 @@ int InptB(string msg)                    // Метод ввода числа в 
     if (succ)
     {
         rslt = Math.Abs(rslt);
-        //return rslt;
+        return rslt;
     }    
     else 
     {
@@ -50,9 +63,9 @@ int InptB(string msg)                    // Метод ввода числа в 
 }
 
 
-int Exponentiation(int A, int B)
+double Exponentiation(double A, int B)
 {
-    int exp = 1;
+    double exp = 1;
     for (int i = 0; i < B; i++)
     {
         exp = exp * A;
