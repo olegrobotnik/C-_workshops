62b5de8de417c6157e6ff1ff94186d232d55d9a9
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-21 19:05:23 +0300

    Added the solved exercise 38

---
+++ exercise38_odd_index_count/Program.cs
@@ -0,0 +1,32 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int[] arry = new int[10];
+int sum = 0;
+int count = 0;
+
+Console.Write("The array: ");
+ArryInit();
+RevealSum();
+
+
+void ArryInit()
+{
+    for (int i = 0; i < arry.Length; i++)
+    {
+        arry[i] = new Random().Next(0, 20);
+        Console.Write($"{arry[i]} ");
+    }
+
+}
+
+
+void RevealSum()
+{
+    while (count < arry.Length)
+    {
+        sum += arry[count];
+        count = count + 2;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"The sum of elements with the odd index is {sum}.");
+}
