ce2ee131045655c2a8a3c93d59a4c964ce28cc89
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-03-16 15:59:59 +0300

    Added solved exercise 46

---
+++ exercise46_shape_scaling/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу масштабирования фигуры
+
+Console.Clear();
+Console.Write("Enter the square scaling factor: ");
+int scale = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+for (int i = 1; i <= scale; i++)
+{
+    for (int j = 1; j <= scale; j++)
+    {
+        Console.Write("# ");
+    }
+    Console.WriteLine();
+}
+Console.ReadLine();
