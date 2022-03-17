4f687df79a056508069aa07f34d0448d8d0eb471
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-03-17 13:49:15 +0300

    Added solved exercise 50

---
+++ exercise50_even_to_negative_in_two-dimensional_array/Program.cs
@@ -0,0 +1,50 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные.
+
+Console.Clear();
+Random rndm = new Random(); 
+int n = rndm.Next(2, 10);
+int k = rndm.Next(2, 10);
+int[,] arry = new int[n, k];
+
+FillArry(arry);
+PrntArry(arry);
+Console.WriteLine();
+EvenToNgtive(arry);
+PrntArry(arry);
+
+void FillArry(int[,] arry)
+{
+    for (int row = 0; row < arry.GetLength(0); row++)
+    {
+        for (int column = 0; column < arry.GetLength(1); column++)
+        {
+            arry[row, column] = rndm.Next(-99, 100);
+        }
+    }
+}
+
+void PrntArry(int[,] arry)
+{
+    for (int row = 0; row < arry.GetLength(0); row++)
+    {
+        for (int column = 0; column < arry.GetLength(1); column++)
+        {
+            Console.Write($"{arry[row, column]} "); 
+        }
+        Console.WriteLine();
+    }
+}
+
+void EvenToNgtive(int[,] arry)
+{
+    for (int row = 0; row < arry.GetLength(0); row++)
+    {
+        for (int column = 0; column < arry.GetLength(1); column++)
+        {
+            if (arry[row, column] % 2 == 0)
+            {
+                arry[row, column] = arry[row, column] * -1;
+            }
+        }
+    }
+}
