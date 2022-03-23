35ae1d9c8ad3b41e2266f9b3e442dd39df34b8c7
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-03-23 17:42:19 +0300

    Added the solved exercise 69

---
+++ exercise69_recursive_numbers_sum/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+
+int M = GetNmbr("Enter the inintial number: "), N = GetNmbr("Enter the final number: ");
+if (M > N)
+{
+    Console.WriteLine();
+    Console.WriteLine("The second number should be greater!");
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine($"The sum of natural numbers from {M} to {N} is {SumRec(M, N)}.");
+}
+
+Console.ReadKey();
+
+int GetNmbr(string txt)
+{
+    Console.Write(txt);
+    string inpt = Console.ReadLine();
+    int.TryParse(inpt, out int nmbr);
+    return nmbr;
+}
+
+int SumRec(int M, int N)
+{
+    if (M <= N)
+    {
+        return M += SumRec(M + 1, N);
+    }
+    else
+    {
+        return 0;
+    }
+}
